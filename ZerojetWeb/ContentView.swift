a339692c2c7c26864a4e070aeeac28c73966f85c
Muny Hove <munyaradzimichaelhove@gmail.com>
2020-07-02 13:19:40 -0400

    Initial Commit

---
+++ ZerojetWeb/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  ZerojetWeb
+//
+//  Created by Muny on 2020-07-02.
+//  Copyright © 2020 Muny. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
